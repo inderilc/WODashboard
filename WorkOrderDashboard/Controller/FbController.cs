5c2f760e16a8e83b6d5be353f563a192f3a26d35
inderilc <inder@israellopezconsulting.com>
2016-09-04 16:06:00 +1000

    Working Code
    Created a Full FbController Class to main API and DB instances

--- WorkOrderDashboard/Controller/FbController.cs
+++ WorkOrderDashboard/Controller/FbController.cs
@@ -3,12 +3,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WorkOrderDashboard.Configuration;
+using FishbowlSDK;
+using FirebirdSql.Data.FirebirdClient;
 
 namespace WorkOrderDashboard.Controller
 {
-    class FbController
+    public class FbController
     {
 
+        private Config cfg;
+        public FbConnection db { get; set; }
+        public FishbowlSDK.Fishbowl api { get; set; }
+        public FbController(Config cfg)
+        {
+            this.cfg = cfg;
+            db = InitDB();
+            api = InitAPI();
+        }
+        private FbConnection InitDB()
+        {
+            String CSB = InitCSB();
+            FbConnection db = new FbConnection(CSB);
+            db.Open();
+            return db;
+        }
 
+        private string InitCSB()
+        {
+            FbConnectionStringBuilder csb = new FbConnectionStringBuilder();
+            csb.DataSource = cfg.Fishbowl.ServerAddress;
+            csb.Database = cfg.Fishbowl.Database;
+            csb.UserID = cfg.Fishbowl.User;
+            csb.Password = cfg.Fishbowl.Pass;
+            csb.Port = cfg.Fishbowl.Port;
+            csb.ServerType = FbServerType.Default;
+            return csb.ToString();
+        }
+
+        private FishbowlSDK.Fishbowl InitAPI()
+        {
+            var newfb = new FishbowlSDK.Fishbowl(cfg.Fishbowl.ServerAddress, cfg.Fishbowl.ServerPort, cfg.Fishbowl.FBIAKey, cfg.Fishbowl.FBIAName, cfg.Fishbowl.FBIADesc, cfg.Fishbowl.Persistent, cfg.Fishbowl.Username, cfg.Fishbowl.Password);
+            return newfb;
+        }
     }
 }
--- WorkOrderDashboard/WorkOrder/WorkOrder.cs
+++ WorkOrderDashboard/WorkOrder/WorkOrder.cs
@@ -16,49 +16,16 @@ namespace WorkOrderDashboard.Models
     {
         public event LogEvent OnLog;
         public delegate void LogEvent(String msg);
-
-        private FbConnection db;
-        public FishbowlSDK.Fishbowl api { get; set; }
+        
+        private FbController fbc {get;set;}
 
         private Config cfg;
 
 
-        /*public WorkOrder(FbConnection db, Config cfg) //Constructor needs to be modified to pass down Config details
-        {
-            this.db = db;
-            this.cfg = cfg;
-        }
-        */
         public WorkOrder(Config cfg)
         {
             this.cfg = cfg;
-            db = InitDB();
-            api = InitAPI();
-        }
-        private FbConnection InitDB()
-        {
-            String CSB = InitCSB();
-            FbConnection db = new FbConnection(CSB);
-            db.Open();
-            return db;
-        }
-
-        private string InitCSB()
-        {
-            FbConnectionStringBuilder csb = new FbConnectionStringBuilder();
-            csb.DataSource = cfg.Fishbowl.ServerAddress;
-            csb.Database = cfg.Fishbowl.Database;
-            csb.UserID = cfg.Fishbowl.User;
-            csb.Password = cfg.Fishbowl.Pass;
-            csb.Port = cfg.Fishbowl.Port;
-            csb.ServerType = FbServerType.Default;
-            return csb.ToString();
-        }
-
-        private FishbowlSDK.Fishbowl InitAPI()
-        {
-            var newfb = new FishbowlSDK.Fishbowl(cfg.Fishbowl.ServerAddress, cfg.Fishbowl.ServerPort, cfg.Fishbowl.FBIAKey, cfg.Fishbowl.FBIAName, cfg.Fishbowl.FBIADesc, cfg.Fishbowl.Persistent, cfg.Fishbowl.Username, cfg.Fishbowl.Password);
-            return newfb;
+            fbc = new FbController(this.cfg);
         }
 
         private void Log(String msg)
@@ -68,10 +35,10 @@ namespace WorkOrderDashboard.Models
                 OnLog(msg);
             }
         }
-
+        
         public ObservableCollection<OrderDataVM> GetWoData()
         {
-            List<OrderData> data = db.Query<OrderData>(SQL.data.showdata).ToList();
+            List<OrderData> data = fbc.db.Query<OrderData>(SQL.data.showdata).ToList();
             if (data != null)
             {
                 return new ObservableCollection<OrderDataVM>(data.Select(k => new OrderDataVM(k)).ToList());
@@ -82,6 +49,7 @@ namespace WorkOrderDashboard.Models
                 return new ObservableCollection<OrderDataVM>();
             }
         }
+        
         public void ChangeDateScheduled(List<string> woList, DateTime toChange)
         {
             Log(string.Format("Changing dates for {0} {1} to {2}.", woList.Count(), woList.Count() < 2 ? "order" : "orders", toChange.ToString("MM-dd-yyyy")));
@@ -89,11 +57,11 @@ namespace WorkOrderDashboard.Models
             {
                 var rq = new GetWorkOrderRqType();
                 rq.WorkOrderNumber = wo.ToString();
-                var rs = api.sendAnyRequest(rq) as GetWorkOrderRsType;
+                var rs = fbc.api.sendAnyRequest(rq) as GetWorkOrderRsType;
                 rs.WO.DateScheduled = toChange.ToString("o");
                 var saverq = new SaveWorkOrderRqType();
                 saverq.WO = rs.WO;
-                var savers = api.sendAnyRequest(saverq) as SaveWorkOrderRsType;
+                var savers = fbc.api.sendAnyRequest(saverq) as SaveWorkOrderRsType;
                 if (savers.statusCode == "1000")
                 {
                     Log("-----Date Changed for Work Order: " + rq.WorkOrderNumber);
